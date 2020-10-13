3ecfbef9532afff3c8969254fe8e251a5725b99b
Marcelo Badet <marcelo.badet@gmail.com>
2020-10-13 15:58:42 -0300

    Criação do projeto

---
+++ src/main/java/com/example/demo/TesteDemoGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDemoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDemoGitApplication.class, args);
+	}
+
+}
